2783772e8a7aec6da604f8a9f99211ab08bdea61
Antonio <nonypot123@gmail.com>
2023-11-20 21:07:37 -0300

    coming of age screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,29 @@
-import { Outlet } from "react-router-dom";
+import { Outlet } from 'react-router-dom';
+
 import { Header } from "./components/NavBar/Header";
 import Footer from "./components/NavBar/Footer";
 import BackButton from "./components/layout/BackButton";
 
+import AgeConfirm from './components/layout/AgeConfirm';
+
+import { useState } from 'react';
+
 import s from './App.module.css'
 
 function App() {
+
+    const [showAgeConfirm, setShowAgeConfirm] = useState(true);
+
+    const handleAgeConfirmClose = () => {
+        setShowAgeConfirm(false);
+    }
+
     return (
         <>
             <Header />
+            {showAgeConfirm && (
+                <AgeConfirm onClose={handleAgeConfirmClose} />
+            )}
 
             <div className={s.Outlet}>
                 <Outlet />
--- src/components/layout/AgeConfirm.jsx
+++ src/components/layout/AgeConfirm.jsx
@@ -2,17 +2,27 @@ import s from './AgeConfirm.module.css'
 
 import Logo from '../public/a.png'
 
-export default function AgeConfirm() {
+export default function AgeConfirm({ onClose }) {
+
+    const ClickButton = (escolha) => {
+        if (escolha === 'Sim') {
+            onClose();
+        } else if (escolha === 'Não') {
+            window.location.href = 'https://www.google.com';
+        }
+    }
+
     return (
         <div className={s.MainContainer}>
-
-            <img className={s.Logo} src={Logo} alt="VinqVinhos" />
-            <div className={s.Title}>
-                <p>Você tem mais de 18 anos?</p>
-            </div>
-            <div className={s.Buttons}>
-                <button>Sim</button>
-                <button>Não</button>
+            <div className={s.Container}>
+                <img className={s.Logo} src={Logo} alt="VinqVinhos" />
+                <div className={s.Title}>
+                    <p>Você tem mais de 18 anos?</p>
+                </div>
+                <div className={s.Buttons}>
+                    <button onClick={() => ClickButton('Não')} className={s.ButtonNo}>Não</button>
+                    <button onClick={() => ClickButton('Sim')} className={s.ButtonYes}>Sim</button>
+                </div>
             </div>
         </div>
     )
